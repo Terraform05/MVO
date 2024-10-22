d2fc87b8f9b832f5eaa8751cec829424c72f5765
Terraform05 <alexjoaramirez@gmail.com>
2024-10-22 14:30:54 -0400

    cleared outputs

--- spy.py
+++
@@ -1,3 +0,0 @@
-
-tickers = ['AAPL', 'MSFT', 'AMZN', 'GOOGL', 'NVDA', 'TSLA', 'GOOG', 'XOM', 'UNH', 'JNJ', 'JPM', 'META', 'V', 'PG', 'HD', 'MA', 'CVX', 'MRK', 'LLY', 'BAC', 'PFE', 'PEP', 'AVGO']
-weights = [6.59, 5.72, 2.53, 1.65, 1.55, 1.54, 1.47, 1.43, 1.24, 1.21, 1.14, 1.08, 0.96, 0.95, 0.91, 0.89, 0.80, 0.79, 0.72, 0.72, 0.70, 70]
